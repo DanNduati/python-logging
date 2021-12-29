f8f19c2a590ea3e79ce8057bbd8ef9ad98a9718e
Daniel Nduati <dannduati2@gmail.com>
2021-12-29 19:51:49 +0300

    Parse variables to logger and update log file

--- simple_logging.py
+++ simple_logging.py
@@ -1,9 +1,15 @@
 import logging
 
+logging.basicConfig(filename="sample.log",level=logging.INFO, format='%(name)s - %(levelname)s - %(asctime)s - %(message)s',datefmt='%d-%b-%y %H:%M:%S')
+
+def logvar():
+    name = 'Dan'
+    logging.error(f'{name} raised an error')
+
 def main():
-    logging.basicConfig(filename="sample.log",level=logging.INFO, format='%(asctime)s - %(message)s',datefmt='%d-%b-%y %H:%M:%S')
-    logging.info("Program started")
-    print("Hello Logging")
+    
+    logging.info("Running main")
+    logvar()
     logging.info("Done!")
 
 if __name__ == "__main__":
