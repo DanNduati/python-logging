8115005b1db20ba30c9faa04d9836d14784ad131
Daniel Nduati <dannduati2@gmail.com>
2021-12-29 19:36:19 +0300

    Update log message format

--- simple_logging.py
+++ simple_logging.py
@@ -1,8 +1,7 @@
 import logging
 
 def main():
-
-    logging.basicConfig(filename="sample.log",level=logging.INFO)
+    logging.basicConfig(filename="sample.log",level=logging.INFO, format='%(name)s - %(levelname)s - %(message)s')
     logging.info("Program started")
     print("Hello Logging")
     logging.info("Done!")
